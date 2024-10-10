670fc695bba1ff141049994a4ac5084c8833cbf4
DeL1TMe <85936407+DeL1TMe@users.noreply.github.com>
2024-10-10 16:18:26 +0300

    Update TabConfigModel.java

--- src/main/java/ru/smole/tabplayerhighlighter/config/TabConfigModel.java
+++ src/main/java/ru/smole/tabplayerhighlighter/config/TabConfigModel.java
@@ -9,5 +9,5 @@ import java.util.List;
 public class TabConfigModel {
     
     @RestartRequired
-    public List<String> players = List.of("65226be8-1539-46fe-b8e5-30b61e5cb3f1");
+    public List<String> players = List.of("UUID");
 }
